d3b816ade1789bc731db1607da5bcce27661c072
jun <yoseb95@gmail.com>
2024-05-31 15:37:21 +0900

    feat: OAuth2UserCustomService 로직 변경 #6 loadUser 의 반환값은 OAuth2User를 구현한 PricipleDetails 로 변경 saveOrUpdate가 아닌 saveOrGet 메소드 이름 수정 saveOrGet 메소드 userInfo를 이용하여 새로운 user를 생성하거나 가져오는 로직으로 변경

--- src/main/java/io/cocotrip/domain/auth/application/OAuth2UserCustomService.java
+++ src/main/java/io/cocotrip/domain/auth/application/OAuth2UserCustomService.java
@@ -1,8 +1,11 @@
 package io.cocotrip.domain.auth.application;
 
+import io.cocotrip.domain.auth.persistence.PrincipalDetails;
 import io.cocotrip.domain.user.persistence.User;
 import io.cocotrip.domain.user.persistence.UserRepository;
-import java.util.Map;
+import io.cocotrip.global.config.oauth.info.OAuth2UserInfo;
+import io.cocotrip.global.config.oauth.info.OAuth2UserInfoFactory;
+import java.util.Optional;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
@@ -11,9 +14,12 @@ import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
+//클래스 위치를 global로 옮겨야 하나?
+
 /**
- * OAuth2UserCustomService OAuth2User를 커스터마이징하는 서비스 클래스
+ * OAuth2UserCustomService OAuth2User를 커스터마이징하는 서비스
  */
+
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 @Service
 public class OAuth2UserCustomService extends DefaultOAuth2UserService {
@@ -27,27 +33,36 @@ public class OAuth2UserCustomService extends DefaultOAuth2UserService {
      */
     @Override
     public OAuth2User loadUser(final OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-        OAuth2User user = super.loadUser(userRequest);
+        OAuth2User oauth2user = super.loadUser(userRequest);
         String oAuthType = userRequest.getClientRegistration().getRegistrationId();
         System.out.println("OAuthType : " + oAuthType);
-        saveOrUpdate(user);
-        return user;
+        OAuth2UserInfo userInfo = OAuth2UserInfoFactory.createOAuth2MemberInfo(oAuthType, oauth2user.getAttributes());
+        User user = saveOrGet(userInfo);
+        return new PrincipalDetails(user, oauth2user.getAttributes());
     }
 
+
     /**
-     * OAuth2User를 받아서 UserRepository에 저장 혹은 닉네임 업데이트하는 메소드
+     * OAuth2UserInfo를 받아서 UserRepository에 저장 혹은 반환하는 메소드
      *
-     * @param oAuth2User OAuth2User 객체
+     * @param oAuth2UserInfo OAuth2UserInfo 객체
      * @return User 객체
      */
-    private User saveOrUpdate(final OAuth2User oAuth2User) {
-        final Map<String, Object> attributes = oAuth2User.getAttributes();
-        for (String key : attributes.keySet()) {
-            System.out.println(key + " : " + attributes.get(key));
-        }
-        String email = (String) attributes.get("email");
-        String name = (String) attributes.get("name");
-        User user = userRepository.findByEmail(email).orElse(User.builder().email(email).nickname(name).build());
-        return userRepository.save(user);
+    private User saveOrGet(final OAuth2UserInfo oAuth2UserInfo) {
+
+        String oauth2Id = oAuth2UserInfo.getProvider() + "_"
+                + oAuth2UserInfo.getProviderId(); //중복이 발생하지 않도록 provider와 providerId를 조합
+        String role = "ROLE_ADMIN"; //일반 유저
+        Optional<User> user = userRepository.findByOauth2Id(oauth2Id);
+        return user.orElseGet(() -> userRepository.save(User.builder()
+                .oauth2Id(oauth2Id)
+                .provider(oAuth2UserInfo.getProvider())
+                .providerId(oAuth2UserInfo.getProviderId())
+                .role(role)
+                .email(oAuth2UserInfo.getEmail())
+                .nickname(oAuth2UserInfo.getName())
+                .build()));
     }
+
+
 }
