4809290a70b01bcf1356b3a9ca7e3ec2db7a1ec5
jun <yoseb95@gmail.com>
2024-06-01 16:29:04 +0900

    feat: AddUserRequest.java setter 삭제 #2

--- src/main/java/io/cocotrip/domain/user/application/dto/AddUserRequest.java
+++ src/main/java/io/cocotrip/domain/user/application/dto/AddUserRequest.java
@@ -1,10 +1,8 @@
 package io.cocotrip.domain.user.application.dto;
 
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 public class AddUserRequest {
     private String email;
     private String password;
