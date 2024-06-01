d85e8dd7d850029af664c4c8296fcaf2569871d5
jun <yoseb95@gmail.com>
2024-06-01 16:33:16 +0900

    feat: CreateAccessTokenRequest Setter 삭제

--- src/main/java/io/cocotrip/domain/auth/application/dto/CreateAccessTokenRequest.java
+++ src/main/java/io/cocotrip/domain/auth/application/dto/CreateAccessTokenRequest.java
@@ -1,10 +1,8 @@
 package io.cocotrip.domain.auth.application.dto;
 
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 public class CreateAccessTokenRequest {
     private String refreshToken;
 }
