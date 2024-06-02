441b1654ced47ecf9f5fb644edff063a1a1beaf7
KangGeonyoung <kky6335@gmail.com>
2024-06-02 22:13:58 +0900

    fix: No serializer found 오류 해결 #4 - GetReviewsResponse에 @Getter 추가

--- src/main/java/com/example/demo/domain/review/application/dto/response/GetReviewsResponse.java
+++ src/main/java/com/example/demo/domain/review/application/dto/response/GetReviewsResponse.java
@@ -2,9 +2,11 @@ package com.example.demo.domain.review.application.dto.response;
 
 import com.example.demo.domain.review.persistence.Review;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 import java.util.List;
 
+@Getter
 @AllArgsConstructor
 public class GetReviewsResponse {
     private List<Review> reviews;
