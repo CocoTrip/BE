1ad1601558488c4257061d6b06e6c6bbba325406
POKUDING <yoseb95@gmail.com>
2024-06-02 23:18:42 +0900

    fix: testcode에서 setter를 사용하여 복구 더 좋은 방안이 있으면 수정필요 및 record로 변경 예정

--- src/main/java/io/cocotrip/domain/auth/application/dto/CreateAccessTokenRequest.java
+++ src/main/java/io/cocotrip/domain/auth/application/dto/CreateAccessTokenRequest.java
@@ -1,8 +1,10 @@
 package io.cocotrip.domain.auth.application.dto;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class CreateAccessTokenRequest {
     private String refreshToken;
 }
