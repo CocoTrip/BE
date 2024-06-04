14bcd0b58aa7a34452b98e4eed2cc0e6cebe4dca
KangGeonyoung <kky6335@gmail.com>
2024-06-05 02:21:43 +0900

    feat: 환경설정(임시) #4 - application.yml: H2 DB 정보 추가 - SecurityConfig: H2 DB가 Security를 통과하도록 허용하는 코드 -> 추후에 삭제될 예정

---
+++ src/main/java/com/example/demo/global/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.example.demo.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    /**
+     *  H2 Security 통과하도록 하는 코드
+     */
+    @Bean
+    @ConditionalOnProperty(name = "spring.h2.console.enabled",havingValue = "true")
+    public WebSecurityCustomizer configureH2ConsoleEnable() {
+        return web -> web.ignoring()
+                .requestMatchers(PathRequest.toH2Console());
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .formLogin().disable()
+                .httpBasic().disable()
+                .authorizeRequests()
+                .requestMatchers("/", "/**").permitAll();
+        return http.build();
+    }
+}
