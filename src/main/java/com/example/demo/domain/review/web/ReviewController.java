9db464f7910e1712f157bf5c08355f0c4c5da96f
KangGeonyoung <kky6335@gmail.com>
2024-06-08 02:57:50 +0900

    feat: RequestMapping 수정 #4 - /reviews -> /api/reviews로 변경

--- src/main/java/com/example/demo/domain/review/web/ReviewController.java
+++ src/main/java/com/example/demo/domain/review/web/ReviewController.java
@@ -15,7 +15,7 @@ import java.util.List;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/reviews")
+@RequestMapping("/api/reviews")
 public class ReviewController {
 
     private final ReviewService reviewService;
